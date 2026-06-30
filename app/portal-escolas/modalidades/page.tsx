49a2d382eff69a70bc8a2d7fdd6905cb085d25ad
silvamplam <silvamplam@gmail.com>
2026-06-30 16:03:16 +0100

    Add files via upload

--- app/portal-escolas/modalidades/page.tsx
+++ app/portal-escolas/modalidades/page.tsx
@@ -447,7 +447,7 @@ function readFormText(formData: FormData, key: string) {
 }
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(value);
 }
 
 function normalizeSlug(value: string) {
